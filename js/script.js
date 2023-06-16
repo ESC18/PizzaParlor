70f49fdb807e4f70c1dad671f3a9cc49d4b7532f
ESC18 <ponderosaprintingus@gmail.com>
2023-06-16 15:10:01 -0700

    finish animations and redo images. Add additional styles. Start working on pizza building area.

--- js/script.js
+++ js/script.js
@@ -18,10 +18,15 @@
 
 
 
-
-
+function showBuilder() {
+    console.log(true)
+    let builderBackground = document.getElementById("builder");
+    builderBackground.style.top = "10vh"; 
+}
 
 
 window.onload = function () {
-    let showBuilderButton = 
+    let showBuilderButton = document.getElementById("showBuilder");
+    showBuilderButton.addEventListener("click", showBuilder)
+
 }
