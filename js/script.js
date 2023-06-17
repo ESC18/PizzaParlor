e4ef06808b68a4f19746743235a6f6db6cd967e7
ESC18 <ponderosaprintingus@gmail.com>
2023-06-17 12:17:22 -0700

    Add price tester function which tests which ingredients are being added and adds to the price. Completes value grabber function. Adds pizza constructor.

--- js/script.js
+++ js/script.js
@@ -1,3 +1,61 @@
+class pizza {
+    constructor(cheese,meat,topOne,topTwo) {
+        this.cheese = cheese;
+        this.meat = meat;
+        this.topOne = topOne;
+        this.topTwo = topTwo;
+    }
+}
+
+
+function priceTester(cheese, meat, topOne, topTwo) {
+    let price = 10;
+    if (cheese === "Mozzerella") {
+        price += 1;
+    }
+    else if (cheese === "Cheddar") {
+        price += 2;
+    }
+    else {
+        cheese = "None";
+        price += 0;
+    }
+    if (meat === "Pepperroni") {
+        price += 2;
+    }
+    else if (meat === "Sausage") {
+        price += 3;
+    }
+    else {
+        meat = "None";
+        price += 0;
+    }
+    if (topOne === "Pineapple") {
+        price += 1;
+    }
+    else if (topOne === "Tomato") {
+        price += 1;
+    }
+    else {
+        topOne = "None";
+        price += 0;
+    }
+    if (topTwo === "Olive") {
+        price += 2;
+    }
+    else if (topTwo === "Pepper") {
+        price += 1;
+    }
+    else {
+        topTwo = "None";
+        price += 0;
+    }
+    let newPizza = new pizza(cheese,meat,topOne,topTwo);
+}
+
+function uiDivGenerator (newPizza) {
+    let div = document.createElement("div") 
+}
 
 
 
@@ -17,6 +75,25 @@
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+function grabValue (event) {
+    event.preventDefault();
+    let cheese = document.getElementById("cheese").value;
+    let meat = document.getElementById("meat").value;
+    let topOne = document.getElementById("topOne").value;
+    let topTwo = document.getElementById("topTwo").value;
+    priceTester(cheese,meat,topOne,topTwo);
+}
 
 function showBuilder() {
     console.log(true)
@@ -24,9 +101,9 @@ function showBuilder() {
     builderBackground.style.top = "10vh"; 
 }
 
-
 window.onload = function () {
+    let form = document.getElementById("pizzaForm");
+    form.addEventListener("submit", grabValue);
     let showBuilderButton = document.getElementById("showBuilder");
     showBuilderButton.addEventListener("click", showBuilder)
-
 }
